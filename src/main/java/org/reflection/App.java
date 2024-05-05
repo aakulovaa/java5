2b30adb323583b20ec63abae9c9f6588b9b06712
annakulova <aakulovaa03@gmail.com>
2024-05-05 15:12:16 +0300

    внедрение зависимостей

--- src/main/java/org/reflection/App.java
+++ src/main/java/org/reflection/App.java
@@ -7,17 +7,22 @@ import java.util.Properties;
 public class App {
     public static void main( String[] args )
     {
-        Properties properties = new Properties();
-        properties.setProperty("org.reflection.interfaces.SomeInterface","org.reflection.classes.SomeImpl");
-        properties.setProperty("org.reflection.interfaces.SomeOtherInterface","org.reflection.classes.SODoer");
+        //Properties properties = new Properties();
+//        properties.setProperty("org.reflection.interfaces.SomeInterface","org.reflection.classes.SomeImpl");
+//        properties.setProperty("org.reflection.interfaces.SomeOtherInterface","org.reflection.classes.SODoer");
+//
+//        FileOutputStream fos;
+//        try{
+//            fos = new FileOutputStream("./src/main/java/org/reflection/properties");
+//            properties.store(fos," ");
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        //SomeBean sb =(new Injector()).inject(new SomeBean());
+        //sb.foo();
 
-        FileOutputStream fos = null;
-        try{
-            fos = new FileOutputStream("./src/main/java/org/reflection/properties");
-            properties.store(fos," ");
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Injector injector = new Injector();
+        injector.inject();
     }
 }
--- src/main/java/org/reflection/Injector.java
+++ src/main/java/org/reflection/Injector.java
@@ -1,7 +1,34 @@
 package org.reflection;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
 public class Injector {
-    public SomeBean inject(SomeBean someBean) {
-        return null;
+    public void inject() {
+
+        Properties properties = new Properties();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("./src/main/java/org/reflection/properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                Objects.requireNonNull(fis).close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        try {
+            System.out.println(Class.forName(properties.getProperty("org.reflection.interfaces.SomeInterface")).newInstance());
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
