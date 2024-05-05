5cee79f1b6512beaa2626494187a10ccb4b1239d
annakulova <aakulovaa03@gmail.com>
2024-05-05 15:40:59 +0300

    внедрение зависимостей в любой объект , который содержит поля помеченные аннотацией

--- src/main/java/org/reflection/App.java
+++ src/main/java/org/reflection/App.java
@@ -1,9 +1,5 @@
 package org.reflection;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class App {
     public static void main( String[] args )
     {
